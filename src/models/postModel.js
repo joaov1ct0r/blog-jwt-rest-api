5ed76a620941fa8edc23092e4688793f3cccdce7
Joao Victor <jov9051@outlook.com>
2022-04-27 15:29:00 -0300

    feat: Post definido em postModel.js

--- src/models/postModel.js
+++ src/models/postModel.js
@@ -1,3 +1,20 @@
 import dbConnection from '../config/database.js';
 
 import { DataTypes } from 'sequelize';
+
+let Post = dbConnection.define('posts', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        unique: true
+    },
+    title: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.STRING(250),
+        allowNull: false
+    }
+});

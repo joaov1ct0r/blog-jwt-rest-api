27e15181291e040300f04acb9acd6ee4ad590e02
Joao Victor <jov9051@outlook.com>
2022-04-28 15:15:04 -0300

    feat: schema definido em validateUserData.js

--- src/controllers/validateUserData.js
+++ src/controllers/validateUserData.js
@@ -1,1 +1,8 @@
 import Joi from '@hapi/joi';
+
+let validateNewUser = data => {
+    let schema = Joi.object({
+        email: Joi.string().required().min(10).max(100),
+        password: Joi.string().required().min(8).max(250)
+    });
+};

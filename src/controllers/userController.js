ee998a673cb6a22d7d8d6154978df8ea528a6668
Joao Victor <jov9051@outlook.com>
2022-04-28 15:26:42 -0300

    feat: validacao de usuario ja registrado ocorrendo em userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -8,4 +8,13 @@ let handleNewUser = async (req, res) => {
     let { error } = validateNewUser(req.body);
 
     if (error) return res.status(400).json({ error });
+
+    let { email, password } = req.body;
+
+    let registeredUser = await User.findOne({
+        where: { email }
+    });
+
+    if (!registeredUser)
+        return res.status(400).json({ error: 'Usuario já cadastrado!' });
 };

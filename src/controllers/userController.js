4102ca5d33bacb4c0b1cac79125bc75bdbd15cfb
Joao Victor <jov9051@outlook.com>
2022-04-28 15:24:36 -0300

    feat: bcrypt importado para userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,6 +2,8 @@ import { validateNewUser } from './validateUserData.js';
 
 import User from '../models/userModel.js';
 
+import bcrypt from 'bcryptjs';
+
 let handleNewUser = async (req, res) => {
     let { error } = validateNewUser(req.body);
 

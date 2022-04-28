4d889a3c9fbbd1b3caac1e7a51df23acd22dd08a
Joao Victor <jov9051@outlook.com>
2022-04-28 15:17:55 -0300

    feat: validacao de req.body

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,1 +1,7 @@
 import { validateNewUser } from './validateUserData.js';
+
+let handleNewUser = async (req, res) => {
+    let { error } = validateNewUser(req.body);
+
+    if (error) return res.status(400).json({ error });
+};

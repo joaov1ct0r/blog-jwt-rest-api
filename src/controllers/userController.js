7352226cb78f03bb5f18d43a1a13c27f12f05e2a
Joao Victor <jov9051@outlook.com>
2022-04-28 15:34:57 -0300

    feat: validateUserData importado para userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,11 @@
-import { validateNewUser } from './validateUserData.js';
+import { validateUserData } from './validateUserData.js';
 
 import User from '../models/userModel.js';
 
 import bcrypt from 'bcryptjs';
 
 let handleNewUser = async (req, res) => {
-    let { error } = validateNewUser(req.body);
+    let { error } = validateUserData(req.body);
 
     if (error) return res.status(400).json({ error });
 

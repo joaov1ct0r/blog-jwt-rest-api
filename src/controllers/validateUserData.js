4978c8149bcbfa5c739f9ec6f9c0644f30a9c87f
Joao Victor <jov9051@outlook.com>
2022-04-28 15:15:53 -0300

    feat: validateNewUser exportado

--- src/controllers/validateUserData.js
+++ src/controllers/validateUserData.js
@@ -5,4 +5,8 @@ let validateNewUser = data => {
         email: Joi.string().required().min(10).max(100),
         password: Joi.string().required().min(8).max(250)
     });
+
+    return schema.validate(data);
 };
+
+export { validateNewUser };

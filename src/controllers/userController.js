0587c736c981205f0c4bcadc2fdb41c9f5429d0a
Joao Victor <jov9051@outlook.com>
2022-04-28 15:23:42 -0300

    feat: User importado para userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,7 @@
 import { validateNewUser } from './validateUserData.js';
 
+import User from '../models/userModel.js';
+
 let handleNewUser = async (req, res) => {
     let { error } = validateNewUser(req.body);
 

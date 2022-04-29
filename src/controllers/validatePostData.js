3e9a28b4dae86c9bc77b167723e95e30722dabeb
Joao Victor <jov9051@outlook.com>
2022-04-29 16:25:49 -0300

    feat: validacao de dados de post ocorrendo em validatePostData.js

--- src/controllers/validatePostData.js
+++ src/controllers/validatePostData.js
@@ -1,1 +1,10 @@
 import Joi from '@hapi/joi';
+
+let validatePostData = data => {
+    let schema = Joi.object({
+        title: Joi.string().required().min(3).max(100),
+        description: Joi.string().required().min(5).max(250)
+    });
+
+    return schema.validate(data);
+};

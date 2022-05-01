972720bbe7955c2073dbea5de9953c3e7d7e35a5
Joao Victor <jov9051@outlook.com>
2022-04-30 21:37:28 -0300

    feat: validacao de token ocorrendo em authController.js

--- src/middlewares/authController.js
+++ src/middlewares/authController.js
@@ -4,4 +4,13 @@ export default function (req, res, next) {
     let token = req.headers['Authorization'];
 
     if (!token) return res.status(401).json({ error: 'Token não encontrado!' });
+
+    try {
+        let verifiedToken = jwt.verify(token, process.env.JWT_TOKEN_SECRET);
+
+        if (!verifiedToken)
+            return res.status(400).json({ error: 'Falha na autenticação!' });
+    } catch (error) {
+        throw error;
+    }
 }

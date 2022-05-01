edab2b6a0e9673051a6b3529cb5019ab5073f824
Joao Victor <jov9051@outlook.com>
2022-04-30 21:24:45 -0300

    feat: metodos de userController.js importado para userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,1 +1,10 @@
 import express from 'express';
+
+import {
+    handleNewUser,
+    handleUserLogin,
+    handleEditUser,
+    handleDeleteUser,
+    handleAllUsers,
+    handleOneUser
+} from '../controllers/userController.js';

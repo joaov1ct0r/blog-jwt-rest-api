3db8fa3a2250096875f67b8d52d779c3d1534a74
Joao Victor <jov9051@outlook.com>
2022-05-01 19:36:51 -0300

    feat: validacao de usuario registrado ocorrendo em admin.js

--- src/middlewares/admin.js
+++ src/middlewares/admin.js
@@ -1,1 +1,9 @@
 import User from '../models/userModel.js';
+
+export default function (req, res, next) {
+  let registeredUser = await User.findOne({
+    where: { email: req.body.email }
+  });
+
+  if(!registeredUser) return res.status(400).json({error: 'Usuario não encontrado'});
+}

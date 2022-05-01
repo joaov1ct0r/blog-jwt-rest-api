574162213478b59f1de5de213c860783e2324565
Joao Victor <jov9051@outlook.com>
2022-04-30 21:35:49 -0300

    feat: validacao de token ocorrendo em authController.js

--- src/middlewares/authController.js
+++ src/middlewares/authController.js
@@ -1,1 +1,7 @@
 import jwt from 'jsonwebtoken';
+
+export default function (req, res, next) {
+    let token = req.headers['Authorization'];
+
+    if (!token) return res.status(401).json({ error: 'Token não encontrado!' });
+}

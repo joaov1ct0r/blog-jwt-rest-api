c3a61b957748f0ba27f5b76f47833dc8f5d6f6f3
Joao Victor <jov9051@outlook.com>
2022-05-03 14:33:23 -0300

    feat: schema de validateHandleDeletePost criado em postController.js

--- src/controllers/validatePostData.js
+++ src/controllers/validatePostData.js
@@ -21,4 +21,11 @@ let validateHandleEditPost = data => {
     return schema.validate(data);
 };
 
+let validateHandleDeletePost = data => {
+    let schema = Joi.object({
+        email: Joi.string().required().min(10).max(100),
+        id: Joi.string().required().min(1).max(10)
+    });
+};
+
 export { validateHandleNewPost, validateHandleEditPost };

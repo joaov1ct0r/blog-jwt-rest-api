c3722bf46a6120b3c11fa49e76258b76517b5e01
Joao Victor <jov9051@outlook.com>
2022-05-03 14:25:33 -0300

    feat: validacao de post ocorrendo em handleEditPost em postController.js

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -51,6 +51,13 @@ let handleEditPost = async (req, res) => {
 
     if (!isUserRegistered)
         return res.status(400).json({ error: 'Usuario não encontrado!' });
+
+    let isPostRegistered = await Post.findOne({
+        where: { id }
+    });
+
+    if (!isPostRegistered)
+        return res.status(400).json({ error: 'Post não encontrado!' });
 };
 
 export { handleNewPost };

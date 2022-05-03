6c833e3606f1214bb715ccb48f233694694aec91
Joao Victor <jov9051@outlook.com>
2022-05-03 14:02:24 -0300

    feat: validacao de body ocorrendo em handleNewPost em postController.js

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -3,3 +3,9 @@ import User from '../models/userModel.js';
 import Post from '../models/postModel.js';
 
 import { validateHandleNewPost } from './validatePostData.js';
+
+let handleNewPost = async (req, res) => {
+    let { error } = validateHandleNewPost(req.body);
+
+    if (error) return res.status(400).json({ error });
+};

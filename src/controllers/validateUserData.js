352368bcd4e064fc98aabfee5feb9cd2460bf654
Joao Victor <jov9051@outlook.com>
2022-05-04 16:34:41 -0300

    feat: password adicionado a validateHandleDeleteUser schema em validateUserData.js

--- src/controllers/validateUserData.js
+++ src/controllers/validateUserData.js
@@ -31,7 +31,8 @@ let validateHandleUserEdit = data => {
 
 let validateHandleDeleteUser = data => {
     let schema = Joi.object({
-        email: Joi.string().required().min(10).max(100)
+        email: Joi.string().required().min(10).max(100),
+        password: Joi.string().required().min(8).max(250)
     });
 
     return schema.validate(data);

e9e006c2feb25daa080848291f90505a36923c9d
Joao Victor <jov9051@outlook.com>
2022-05-05 14:30:20 -0300

    feat: metodos de postController.js importado para postRoutes.js

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,1 +1,9 @@
 import express from 'express';
+
+import {
+    handleNewPost,
+    handleEditPost,
+    handleDeletePost,
+    handleAllPosts,
+    handleOnePost
+} from '../controllers/postController.js';

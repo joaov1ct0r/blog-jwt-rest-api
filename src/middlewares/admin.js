9526b84b6691a584c5a506ac3830d7b38d803773
Joao Victor <jov9051@outlook.com>
2022-05-05 14:58:11 -0300

    refactor: async adicinado a function em admin.js

--- src/middlewares/admin.js
+++ src/middlewares/admin.js
@@ -1,23 +1,28 @@
 import User from '../models/userModel.js';
 
-export default function (req, res, next) {
-  let { email } = req.body;
+export default async function (req, res, next) {
+    let { email } = req.body;
 
-  if(!email) return res.status(400).json({error: 'Email não encontrado na requisição!'})
+    if (!email)
+        return res
+            .status(400)
+            .json({ error: 'Email não encontrado na requisição!' });
 
-  let isUserRegistered = await User.findOne({
-    where: { email }
-  });
+    let isUserRegistered = await User.findOne({
+        where: { email }
+    });
 
-  if(!isUserRegistered) return res.status(400).json({error: 'Usuario não encontrado!'});
+    if (!isUserRegistered)
+        return res.status(400).json({ error: 'Usuario não encontrado!' });
 
-  try {
-    let isUserAdmin = isUserRegistered.admin === true ? true : false;
-    
-    if(!isUserAdmin) return res.status(401).json({error: 'Não autorizado!'})
+    try {
+        let isUserAdmin = isUserRegistered.admin === true ? true : false;
 
-    next()
-  } catch (error) {
-    throw error;
-  }
+        if (!isUserAdmin)
+            return res.status(401).json({ error: 'Não autorizado!' });
+
+        next();
+    } catch (error) {
+        throw error;
+    }
 }

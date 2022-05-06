b9309094a935bdce1926a6d49964db51d6be1a27
Joao Victor <jov9051@outlook.com>
2022-05-06 19:28:31 -0300

    feat: userNewEmail e userNewPassword adicionado a validateHandleAdminEditUser schema em validateAdminData.js

--- src/controllers/validateAdminData.js
+++ src/controllers/validateAdminData.js
@@ -5,7 +5,9 @@ let validateHandleAdminEditUser = data => {
         email: Joi.string().required().min(10).max(100),
         password: Joi.string().required().min(8).max(250),
         userEmail: Joi.string().required().min(10).max(100),
-        userPassword: Joi.string().required().min(8).max(250)
+        userPassword: Joi.string().required().min(8).max(250),
+        userNewEmail: Joi.string().required().min(10).max(100),
+        userNewPassword: Joi.string().required().min(8).max(250)
     });
 
     return schema.validate(data);

694f9a40ab5d782b99ee230c7beba1033fc264f2
Joao Victor <jov9051@outlook.com>
2022-05-06 19:04:30 -0300

    feat: schema de validateAdminDeleteUser criado em validateAdminData.js

--- src/controllers/validateAdminData.js
+++ src/controllers/validateAdminData.js
@@ -11,4 +11,12 @@ let validateAdminEditUser = data => {
     return schema.validate(data);
 };
 
+let validateAdminDeleteUser = data => {
+    let schema = Joi.object({
+        email: Joi.string().required().min(10).max(100),
+        password: Joi.string().required().min(8).max(250),
+        userEmail: Joi.string().required().min(10).max(100)
+    });
+};
+
 export { validateAdminEditUser };

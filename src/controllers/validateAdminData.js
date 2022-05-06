f599954292ad34700a25c197e3999aa46d6c4f33
Joao Victor <jov9051@outlook.com>
2022-05-06 19:02:20 -0300

    feat: schema de validateAdminEditUser criado em validateAdminData.js

--- src/controllers/validateAdminData.js
+++ src/controllers/validateAdminData.js
@@ -1,1 +1,10 @@
 import Joi from '@hapi/joi';
+
+let validateAdminEditUser = data => {
+    let schema = Joi.object({
+        email: Joi.string().required().min(10).max(100),
+        password: Joi.string().required().min(8).max(250),
+        userEmail: Joi.string().required().min(10).max(100),
+        userPassword: Joi.string().required().min(8).max(250)
+    });
+};

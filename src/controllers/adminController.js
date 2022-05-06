483e81ea18480b378b4a89404cbdd9420f747fc1
Joao Victor <jov9051@outlook.com>
2022-05-06 19:19:57 -0300

    feat: metodos de validateAdminData.js importados para adminController.js

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -3,3 +3,9 @@ import User from '../models/userModel.js';
 import Post from '../models/postModel.js';
 
 import bcrypt from 'bcryptjs';
+
+import {
+    validateAdminEditUser,
+    validateAdminDeleteUser,
+    validateAdminDeletePost
+} from '../controllers/validateAdminData.js';

48a9193a8ecde624cbc341c158643d0bec38b4f0
Joao Victor <jov9051@outlook.com>
2022-05-08 21:49:51 -0300

    refactor: postId em validateAdminData.js em validateHandleAdminDeletePost renomeado para id

--- src/controllers/validateAdminData.js
+++ src/controllers/validateAdminData.js
@@ -26,7 +26,7 @@ let validateHandleAdminDeletePost = data => {
     let schema = Joi.object({
         email: Joi.string().required().min(10).max(100),
         password: Joi.string().required().min(8).max(250),
-        postId: Joi.string().required().min(1)
+        id: Joi.string().required().min(1)
     });
 
     return schema.validate(data);

f73b779b9f655e56c1e18d8af19e32b819de1135
Joao Victor <jov9051@outlook.com>
2022-05-10 17:20:27 -0300

    feat: schema de validateHandleAdminDeletePost redefinido

--- src/controllers/validateAdminData.js
+++ src/controllers/validateAdminData.js
@@ -20,8 +20,6 @@ let validateHandleAdminDeleteUser = data => {
 
 let validateHandleAdminDeletePost = data => {
     let schema = Joi.object({
-        email: Joi.string().required().min(10).max(100),
-        password: Joi.string().required().min(8).max(250),
         id: Joi.string().required().min(1)
     });
 

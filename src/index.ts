6b6dcc35d474f910ed00acfe86fadf86a197785c
Joao Victor <jov9051@outlook.com>
2022-05-16 15:58:36 -0300

    refactor: indentation alterada para 2espacos e aspas duplas aderidas

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import 'dotenv/config';
+import "dotenv/config";
 
-import cors from 'cors';
+import cors from "cors";
 
-import express, { Express } from 'express';
+import express, { Express } from "express";
 
-import postRouter from './routes/postRoutes.js';
+import postRouter from "./routes/postRoutes.js";
 
-import userRouter from './routes/userRoutes.js';
+import userRouter from "./routes/userRoutes.js";
 
-import adminRouter from './routes/adminRoutes.js';
+import adminRouter from "./routes/adminRoutes.js";
 
 const app: Express = express();
 
@@ -18,12 +18,12 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/user', userRouter);
+app.use("/api/user", userRouter);
 
-app.use('/api/post', postRouter);
+app.use("/api/post", postRouter);
 
-app.use('/api/admin', adminRouter);
+app.use("/api/admin", adminRouter);
 
 app.listen(process.env.SERVER_PORT, () => {
-    console.log('Server running!');
+  console.log("Server running!");
 });

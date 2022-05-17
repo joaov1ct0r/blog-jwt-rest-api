eeaf2e20014f1e905c725be352e2291a432d4b2f
Joao Victor <jov9051@outlook.com>
2022-05-17 17:08:52 -0300

    refactor: const dbConnection renomeado para DB

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-export const dbConnection: Sequelize = new Sequelize(String(process.env.DB_DATABASE), String(process.env.DB_USER), String(process.env.DB_PASSWORD), {
+export const DB: Sequelize = new Sequelize(String(process.env.DB_DATABASE), String(process.env.DB_USER), String(process.env.DB_PASSWORD), {
   dialect: "mysql",
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT)

57c9f2ff4765a67c4e3195ef4dc6f1baa9c15d86
Joao Victor <jov9051@outlook.com>
2022-05-17 14:42:03 -0300

    feat: const dbConnection tipada em database.ts

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,22 +1,7 @@
-import sequelize from 'sequelize';
+import { Sequelize } from "sequelize";
 
-let { Sequelize } = sequelize;
-
-let dbConnection = new Sequelize({
-    dialect: 'mysql',
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT
+export const dbConnection: Sequelize = new Sequelize(String(process.env.DB_DATABASE), String(process.env.DB_USER), String(process.env.DB_PASSWORD), {
+  dialect: "mysql",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT)
 });
-
-try {
-    await dbConnection.authenticate();
-
-    console.log('Conexão com DB estabelecida!');
-} catch (error) {
-    throw error;
-}
-
-export default dbConnection;

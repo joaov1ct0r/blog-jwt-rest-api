818f48ab493dd2ce0f86b8f18bd9c0709e502e89
Joao Victor <jov9051@outlook.com>
2022-05-18 13:51:45 -0300

    refactor: var DB em database/config/database.ts tipada

--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -1,7 +1,9 @@
-import { Sequelize } from "sequelize";
+import sequelize from "sequelize";
 
-export const DB: Sequelize = new Sequelize(String(process.env.DB_DATABASE), String(process.env.DB_USER), String(process.env.DB_PASSWORD), {
+const DB: sequelize.Sequelize = new sequelize.Sequelize(String(process.env.DB_DATABASE), String(process.env.DB_USER), String(process.env.DB_PASSWORD), {
   dialect: "mysql",
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT)
 });
+
+export default DB;

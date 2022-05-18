2306d788d664ed509c418913e684d56fb7a969f8
Joao Victor <jov9051@outlook.com>
2022-05-18 13:57:08 -0300

    refactor: var Post em postModel.ts tipada em postModel.ts

--- src/database/models/postModel.ts
+++ src/database/models/postModel.ts
@@ -4,46 +4,46 @@ import User from "../models/userModel";
 
 import { DataTypes } from "sequelize/types";
 
-let Post = dbConnection.define(
-    'posts',
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true,
-            unique: true
-        },
-        author: {
-            type: DataTypes.STRING(100),
-            allowNull: false
-        },
-        title: {
-            type: DataTypes.STRING(100),
-            allowNull: false
-        },
-        description: {
-            type: DataTypes.STRING(250),
-            allowNull: false
-        },
-        content: {
-            type: DataTypes.STRING(250),
-            allowNull: false
-        }
+const Post = DB.define(
+  "posts",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      unique: true
     },
-    {
-        freezeTableName: true,
-        tableName: 'posts',
-        timestamps: false
+    author: {
+      type: DataTypes.STRING(100),
+      allowNull: false
+    },
+    title: {
+      type: DataTypes.STRING(100),
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING(250),
+      allowNull: false
+    },
+    content: {
+      type: DataTypes.STRING(250),
+      allowNull: false
     }
+  },
+  {
+    freezeTableName: true,
+    tableName: "posts",
+    timestamps: false
+  }
 );
 
 Post.belongsTo(User, {
-    constraints: true,
-    foreignKey: 'userId'
+  constraints: true,
+  foreignKey: "userId"
 });
 
 User.hasMany(Post, {
-    foreignKey: 'userId'
+  foreignKey: "userId"
 });
 
 Post.sync();

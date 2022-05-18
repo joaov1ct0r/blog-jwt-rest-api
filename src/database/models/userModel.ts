2841688b0481f9d1a9e54a6b55a311d8daade9d8
Joao Victor <jov9051@outlook.com>
2022-05-18 13:50:07 -0300

    refactor: var User em userModel.ts tipado

--- src/database/models/userModel.ts
+++ src/database/models/userModel.ts
@@ -1,35 +1,35 @@
-import DB from "../config/database.js"
+import DB from "../config/database.js";
 
-import { DataTypes } from 'sequelize';
+import { DataTypes } from "sequelize/types";
 
-let User = dbConnection.define(
-    'users',
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            unique: true,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        email: {
-            type: DataTypes.STRING(100),
-            unique: true,
-            allowNull: false
-        },
-        password: {
-            type: DataTypes.STRING(250),
-            allowNull: false
-        },
-        admin: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false
-        }
+const User = DB.define(
+  "users",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      unique: true,
+      primaryKey: true,
+      autoIncrement: true
     },
-    {
-        freezeTableName: true,
-        tableName: 'users',
-        timestamps: false
+    email: {
+      type: DataTypes.STRING(100),
+      unique: true,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING(250),
+      allowNull: false
+    },
+    admin: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
     }
+  },
+  {
+    freezeTableName: true,
+    tableName: "users",
+    timestamps: false
+  }
 );
 
 User.sync();

3b2f92a552a33a960aeca13e533b5edd4854635f
Joao Victor <jov9051@outlook.com>
2022-05-19 17:14:26 -0300

    refactor: vars em postROutes.ts tipadas

--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -1,25 +1,25 @@
-import express from 'express';
+import express from "express";
 
 import {
-    handleNewPost,
-    handleEditPost,
-    handleDeletePost,
-    handleAllPosts,
-    handleOnePost
-} from '../controllers/postController.js';
+  handleNewPost,
+  handleEditPost,
+  handleDeletePost,
+  handleAllPosts,
+  handleOnePost
+} from "../controllers/postController.js";
 
-import auth from '../middlewares/auth.js';
+import auth from "../middlewares/auth.js";
 
-let postRouter = express.Router();
+const postRouter: express.Router = express.Router();
 
-postRouter.post('/register', auth, handleNewPost);
+postRouter.post("/register", auth, handleNewPost);
 
-postRouter.put('/edit', auth, handleEditPost);
+postRouter.put("/edit", auth, handleEditPost);
 
-postRouter.delete('/delete', auth, handleDeletePost);
+postRouter.delete("/delete", auth, handleDeletePost);
 
-postRouter.get('/posts', auth, handleAllPosts);
+postRouter.get("/posts", auth, handleAllPosts);
 
-postRouter.get('/post', auth, handleOnePost);
+postRouter.get("/post", auth, handleOnePost);
 
 export default postRouter;

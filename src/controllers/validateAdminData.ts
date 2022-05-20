5760c8cf3746a3dcad2057c92819638115cedffd
Joao Victor <jov9051@outlook.com>
2022-05-20 16:38:21 -0300

    refactor: var validateHandleAdminEditUser tipada em validateAdminData.ts

--- src/controllers/validateAdminData.ts
+++ src/controllers/validateAdminData.ts
@@ -1,7 +1,7 @@
 import Joi from "@hapi/joi";
 
 const validateHandleAdminEditUser = (data: Object): Joi.ValidationResult => {
-  const schema: Joi.ObjectSchema = Joi.object({
+  const schema: Joi.ObjectSchema<Object> = Joi.object({
     userEmail: Joi.string().required().min(10).max(100),
     userNewEmail: Joi.string().required().min(10).max(100),
     userNewPassword: Joi.string().required().min(8).max(250)

413938fb129703301ce62826a2fc1e6ea6f36e73
Joao Victor <jov9051@outlook.com>
2022-05-20 16:29:16 -0300

    refactor: var validateHandleNewPost tipada em validatePostData.ts

--- src/controllers/validatePostData.ts
+++ src/controllers/validatePostData.ts
@@ -1,45 +1,45 @@
-import Joi from '@hapi/joi';
+import Joi from "@hapi/joi";
 
-let validateHandleNewPost = data => {
-    let schema = Joi.object({
-        title: Joi.string().required().min(3).max(100),
-        description: Joi.string().required().min(5).max(250),
-        content: Joi.string().required().min(10).max(250)
-    });
+const validateHandleNewPost = (data: Object): Joi.ValidationResult => {
+  const schema: Joi.ObjectSchema = Joi.object({
+    title: Joi.string().required().min(3).max(100),
+    description: Joi.string().required().min(5).max(250),
+    content: Joi.string().required().min(10).max(250)
+  });
 
-    return schema.validate(data);
+  return schema.validate(data);
 };
 
 let validateHandleEditPost = data => {
-    let schema = Joi.object({
-        title: Joi.string().required().min(3).max(100),
-        description: Joi.string().required().min(5).max(250),
-        content: Joi.string().required().min(10).max(250),
-        id: Joi.string().required().min(1).max(10)
-    });
+  let schema = Joi.object({
+    title: Joi.string().required().min(3).max(100),
+    description: Joi.string().required().min(5).max(250),
+    content: Joi.string().required().min(10).max(250),
+    id: Joi.string().required().min(1).max(10)
+  });
 
-    return schema.validate(data);
+  return schema.validate(data);
 };
 
 let validateHandleDeletePost = data => {
-    let schema = Joi.object({
-        id: Joi.string().required().min(1).max(10)
-    });
+  let schema = Joi.object({
+    id: Joi.string().required().min(1).max(10)
+  });
 
-    return schema.validate(data);
+  return schema.validate(data);
 };
 
 let validateHandleOnePost = data => {
-    let schema = Joi.object({
-        id: Joi.string().required().min(1).max(10)
-    });
+  let schema = Joi.object({
+    id: Joi.string().required().min(1).max(10)
+  });
 
-    return schema.validate(data);
+  return schema.validate(data);
 };
 
 export {
-    validateHandleNewPost,
-    validateHandleEditPost,
-    validateHandleDeletePost,
-    validateHandleOnePost
+  validateHandleNewPost,
+  validateHandleEditPost,
+  validateHandleDeletePost,
+  validateHandleOnePost
 };

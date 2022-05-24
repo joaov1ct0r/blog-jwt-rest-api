58415de945f26db2a8669b77cb3cfe7085945020
Joao Victor <jov9051@outlook.com>
2022-05-24 14:07:09 -0300

    refactor: as sendo utilizado em database.ts

--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -1,6 +1,6 @@
 import sequelize from "sequelize";
 
-const DB: sequelize.Sequelize = new sequelize.Sequelize(String(process.env.DB_DATABASE), String(process.env.DB_USER), String(process.env.DB_PASSWORD), {
+const DB: sequelize.Sequelize = new sequelize.Sequelize(process.env.DB_DATABASE as string, process.env.DB_USER as string, process.env.DB_PASSWORD as string, {
   dialect: "mysql",
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT)

64bcd21303a39b1b3103439093ce25d188394512
Joao Victor <jov9051@outlook.com>
2022-05-25 19:13:16 -0300

    refactor: http code em auth.ts alterado

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -9,12 +9,12 @@ import IReq from "../types/requestInterface";
 export default function (req: IReq, res: Response, next: NextFunction) {
   const token = req.headers.authorization!.split(" ")[1];
 
-  if (!token) return res.status(401).json({ error: "Token não encontrado!" });
+  if (!token) return res.status(400).json({ error: "Token não encontrado!" });
 
   const verifiedToken = jwt.verify(token, process.env.JWT_TOKEN_SECRET as string) as IJwt;
 
   if (!verifiedToken) {
-    return res.status(400).json({ error: "Falha na autenticação!" });
+    return res.status(401).json({ error: "Falha na autenticação!" });
   }
 
   req.userId = verifiedToken.id;

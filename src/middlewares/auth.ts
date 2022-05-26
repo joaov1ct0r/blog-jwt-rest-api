043a3cb38428900adc6fb48dcf461bac26e3b47b
Joao Victor <jov9051@outlook.com>
2022-05-25 22:18:36 -0300

    refactor: auth.ts tipado

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -7,11 +7,11 @@ import { Response, NextFunction } from "express";
 import IReq from "../types/requestInterface";
 
 export default function (req: IReq, res: Response, next: NextFunction) {
-  const token = req.headers.authorization!.split(" ")[1];
+  const token: string = req.headers.authorization!.split(" ")[1];
 
   if (!token) return res.status(400).json({ error: "Token não encontrado!" });
 
-  const verifiedToken = jwt.verify(token, process.env.JWT_TOKEN_SECRET as string) as IJwt;
+  const verifiedToken: IJwt = jwt.verify(token, process.env.JWT_TOKEN_SECRET as string) as IJwt;
 
   if (!verifiedToken) {
     return res.status(401).json({ error: "Falha na autenticação!" });

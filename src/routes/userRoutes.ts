e3af114f1a26e03c74b70e7bda12a1bcff0a6482
Joao Victor <jov9051@outlook.com>
2022-07-19 19:22:57 -0300

    refactor: metodos de UserController adicionados a userRoutes.ts

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,21 +1,21 @@
 import express from "express";
 
-import userController from "../controllers/userController";
+import UserController from "../controllers/userController";
 
 import auth from "../middlewares/auth";
 
 const userRouter: express.Router = express.Router();
 
-userRouter.post("/register", userController.handleNewUser);
+userRouter.post("/register", UserController.handleNewUser);
 
-userRouter.post("/login", userController.handleUserLogin);
+userRouter.post("/login", UserController.handleUserLogin);
 
-userRouter.put("/edit", auth, userController.handleEditUser);
+userRouter.put("/edit", auth, UserController.handleEditUser);
 
-userRouter.delete("/delete", auth, userController.handleDeleteUser);
+userRouter.delete("/delete", auth, UserController.handleDeleteUser);
 
-userRouter.get("/users", auth, userController.handleAllUsers);
+userRouter.get("/users", auth, UserController.handleAllUsers);
 
-userRouter.get("/user", auth, userController.handleOneUser);
+userRouter.get("/user", auth, UserController.handleOneUser);
 
 export default userRouter;

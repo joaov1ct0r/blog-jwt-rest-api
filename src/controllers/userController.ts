1d5d4f3e0c9f86b50dc55b239514ffea7cfed044
Joao Victor <jov9051@outlook.com>
2022-07-19 17:48:24 -0300

    feat: metodos de userController.ts inseridos em class UserController

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -94,7 +94,7 @@ class UserController {
     }
   }
 
-  async handleEditUser(req: Request, res: Response): Promise<Response> {
+  async handleEditUser(req: IReq, res: Response): Promise<Response> {
     const { error } = validateHandleUserEdit(req.body);
 
     if (error) return res.status(400).json({ error });
@@ -126,10 +126,7 @@ class UserController {
     }
   }
 
-  handleDeleteUser = async (
-    req: IReq,
-    res: Response
-  ): Promise<Response<any, Record<string, any>>> => {
+  async handleDeleteUser(req: IReq, res: Response): Promise<Response> {
     const id: string | undefined = req.userId;
 
     try {
@@ -150,115 +147,30 @@ class UserController {
     } catch (err: unknown) {
       return res.status(500).json({ err });
     }
-  };
-}
+  }
 
-const handleEditUser = async (
-  req: IReq,
-  res: Response
-): Promise<Response<any, Record<string, any>>> => {
-  const { error } = validateHandleUserEdit(req.body);
+  async handleOneUser(req: Request, res: Response): Promise<Response> {
+    const { error } = validateHandleOneUser(req.body);
 
-  if (error) return res.status(400).json({ error });
+    if (error) return res.status(400).json({ error });
 
-  const email: string = req.body.email;
+    const email: string = req.body.email;
 
-  const password: string = req.body.password;
+    try {
+      const isUserRegistered: IUser | null = await User.findOne({
+        include: Post,
+        where: { email },
+      });
 
-  const id: string | undefined = req.userId;
-
-  try {
-    const editedUser: [affectedCount: number] = await User.update(
-      {
-        email,
-        password: bcrypt.hashSync(password),
-      },
-      {
-        where: { id },
+      if (isUserRegistered === null) {
+        return res.status(404).json({ error: "Usuario não encontrado!" });
       }
-    );
 
-    if (editedUser[0] === 0) {
-      return res.status(500).json({ error: "Falha ao atualizar usuario!" });
+      return res.status(200).json({ isUserRegistered });
+    } catch (err: unknown) {
+      return res.status(500).json({ err });
     }
-
-    return res.status(204).send();
-  } catch (err: unknown) {
-    return res.status(500).json({ err });
   }
-};
-
-const handleDeleteUser = async (
-  req: IReq,
-  res: Response
-): Promise<Response<any, Record<string, any>>> => {
-  const id: string | undefined = req.userId;
-
-  try {
-    const deletedUser: number = await User.destroy({
-      where: { id },
-    });
-
-    if (deletedUser === 0) {
-      return res.status(500).json({ error: "Falha ao deletar usuario!" });
-    }
-
-    // eslint-disable-next-line no-unused-vars
-    const deletedPosts: number = await Post.destroy({
-      where: { userId: id },
-    });
-
-    return res.status(204).send();
-  } catch (err: unknown) {
-    return res.status(500).json({ err });
-  }
-};
-
-const handleAllUsers = async (req: Request, res: Response) => {
-  try {
-    const users: IUser[] = await User.findAll({
-      include: Post,
-    });
-
-    return res.status(200).json({ users });
-  } catch (err: unknown) {
-    return res.status(500).json({ err });
-  }
-};
-
-const handleOneUser = async (
-  req: Request,
-  res: Response
-): Promise<Response<any, Record<string, any>>> => {
-  const { error } = validateHandleOneUser(req.body);
-
-  if (error) return res.status(400).json({ error });
-
-  const email: string = req.body.email;
-
-  try {
-    const isUserRegistered: IUser | null = await User.findOne({
-      include: Post,
-      where: { email },
-    });
-
-    if (isUserRegistered === null) {
-      return res.status(404).json({ error: "Usuario não encontrado!" });
-    }
-
-    return res.status(200).json({ isUserRegistered });
-  } catch (err: unknown) {
-    return res.status(500).json({ err });
-  }
-};
-
-export {
-  handleNewUser,
-  handleUserLogin,
-  handleEditUser,
-  handleDeleteUser,
-  handleAllUsers,
-  handleOneUser,
-};
+}
 
 export default new UserController();
